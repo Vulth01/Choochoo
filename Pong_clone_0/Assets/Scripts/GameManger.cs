9bacfebb91c9daff1b24145e431090d4b0d036c7
VaderPorpoise <76525513+VaderPorpoise@users.noreply.github.com>
2021-03-03 22:16:12 +0200

    stillParrelsync

---
+++ Pong_clone_0/Assets/Scripts/GameManger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    Rigidbody ballBody;
+    float startSpeed = 500;
+    GameObject ball;
+    public Transform ballStartPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballBody = ball.GetComponent<Rigidbody>();
+        StartCoroutine(GameStartup());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballBody.transform.position.y < -5)
+        {
+
+
+        }
+    }
+    IEnumerator GameStartup()
+    {
+        Instantiate(ball, ballStartPoint);
+        yield return new WaitForSeconds(1);
+        Debug.Log("3");
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+        Debug.Log("start");
+
+        ballBody.AddForce(new Vector3(0, 0, -startSpeed));
+
+    }
+
+}
