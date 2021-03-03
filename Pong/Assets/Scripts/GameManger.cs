739237769a246975d521d3521947784d85af1dcf
VaderPorpoise <76525513+VaderPorpoise@users.noreply.github.com>
2021-03-03 18:34:31 +0200

    added the ping pong demo but still need to fix the ball
    its dark

---
+++ Pong/Assets/Scripts/GameManger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public Rigidbody ballBody;
+    float startSpeed = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GameStartup());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator GameStartup()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("3");
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+        Debug.Log("start");
+
+        ballBody.AddForce(new Vector3(0, 0, -startSpeed));
+
+    }
+
+}
