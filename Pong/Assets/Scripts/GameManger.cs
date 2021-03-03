a810f6b9c88f02365e57d6e7560001aadce2cb64
VaderPorpoise <76525513+VaderPorpoise@users.noreply.github.com>
2021-03-03 21:55:57 +0200

    finished 1 player pong
    need to change camera, visuals and make multiplayer

--- Pong/Assets/Scripts/GameManger.cs
+++ Pong/Assets/Scripts/GameManger.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
-    public Rigidbody ballBody;
+    Rigidbody ballBody;
     float startSpeed = 500;
-
+    GameObject ball;
+    public Transform ballStartPoint;
     // Start is called before the first frame update
     void Start()
     {
+        ballBody = ball.GetComponent<Rigidbody>();
         StartCoroutine(GameStartup());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ballBody.transform.position.y < -5)
+        {
 
+
+        }
     }
     IEnumerator GameStartup()
     {
+        Instantiate(ball, ballStartPoint);
         yield return new WaitForSeconds(1);
         Debug.Log("3");
         yield return new WaitForSeconds(1);
